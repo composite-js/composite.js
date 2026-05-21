fed820803bfabc3fe138475987fefae3a91c5e44
Zhiyang Shen <chilyangshen@outlook.com>
2026-05-21 04:58:47 +0000

    test: format axis renderer call

--- test/axis.test.mjs
+++ test/axis.test.mjs
@@ -12,11 +12,15 @@ import { createFakeSvg } from "./helpers/fake-svg.mjs";
   });
   const x = d3.scaleLinear().domain([0, 9000]).range([120, 0]);
 
-  renderer.render(svg, { x }, {
-    margin: { top: 30, right: 0, bottom: 0, left: 10 },
-    width: 120,
-    height: 80,
-  });
+  renderer.render(
+    svg,
+    { x },
+    {
+      margin: { top: 30, right: 0, bottom: 0, left: 10 },
+      width: 120,
+      height: 80,
+    },
+  );
 
   const label = svg
     .querySelectorAll("text")
